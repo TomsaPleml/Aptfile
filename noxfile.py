f670151072be361df4c5a75df86b8a3ebecde38a
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-01 11:44:00 +0100

    Create noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,43 @@
+# from: https://github.com/pradyunsg/furo/blob/main/noxfile.py
+# license: https://github.com/pradyunsg/furo/blob/main/LICENSE
+
+"""Development automation
+"""
+import glob
+import os
+
+import nox
+
+#
+# Development Sessions
+#
+@nox.session(reuse_venv=True)
+def docs(session):
+    # Generate documentation into `build/docs`
+    session.run("sphinx-build", "-b", "dirhtml", "-v", "documentation/source/", "build/docs")
+
+
+@nox.session(name="docs-live", reuse_venv=True)
+def docs_live(session):
+    # Generate documentation into `build/docs`
+    session.run("stb", "serve", "docs/")
+
+
+@nox.session(reuse_venv=True)
+def lint(session):
+    session.install("pre-commit")
+
+    args = list(session.posargs)
+    args.append("--all-files")
+    if "CI" in os.environ:
+        args.append("--show-diff-on-failure")
+
+    session.run("pre-commit", "run", *args)
+
+@nox.session
+def compile(session):
+    print("Installing PyInstaller...")
+    session.install("PyInstaller")
+    print("Compiling...")
+    os.system("pyinstaller --name so --console aptfile.py")
+    print("Done!")
