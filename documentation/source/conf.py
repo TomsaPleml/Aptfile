df135b3060ed635c8c73decbef47cb39546d4a34
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-01 14:40:49 +0100

    Update conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
   "sphinx_copybutton",
   "myst_parser",
   "sphinx_pyscript",
+  "sphinx_multi_theme.multi_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
