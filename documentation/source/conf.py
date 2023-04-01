6207d54aa4cb330469bd8cd87faedc6c9ba620d6
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-01 15:34:43 +0100

    Update conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -24,6 +24,7 @@ author = 'TylerMS887'
 
 # The full version, including alpha/beta/rc tags
 release = 'main'
+master_doc = "index"
 
 
 # -- General configuration ---------------------------------------------------
