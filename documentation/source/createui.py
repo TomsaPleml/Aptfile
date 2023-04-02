588ad060fdcc361fa54ec6bc373aa2fbd91fa4b5
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-02 15:07:58 +0100

    Update createui.py

--- documentation/source/createui.py
+++ documentation/source/createui.py
@@ -1,4 +1,6 @@
 from js import document
 version = "0.1-alpha"
 version_label = document.querySelector("span#aptfile-version")
+toml_code = document.querySelector("span#toml")
 version_label.textContent = f"Using Aptfile {version}"
+toml_code.textContent = f"Aptfile {version} generator test."
