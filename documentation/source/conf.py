91f57f594bff7ab9b40575471ddb50e0a8b68c80
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-02 13:54:49 +0100

    Update conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -37,7 +37,6 @@ extensions = [
   "sphinx_copybutton",
   "myst_parser",
   "sphinx_pyscript",
-  "sphinxawesome_theme",
 ]
 myst_enable_extensions = [
     "colon_fence",
@@ -59,7 +58,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinxawesome_theme"
+html_theme = "sphinx_material"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
